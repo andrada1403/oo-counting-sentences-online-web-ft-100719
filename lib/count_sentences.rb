f0ba483d912226c331f2c1fabf04c93056e7ddde
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-12 14:57:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,16 +27,16 @@ class String
   end
 
   def count_sentences
-    sentence_array=self.split(".")
-    sentence_array.each do |word|
-      if word.include?("!")
-        word.split("!").each do |str|
-          if str !=""
-            sentence_array<<str
-          end
-        end
-      end
-    end
+    #sentence_array=self.split(".")
+    #sentence_array.each do |word|
+      #if word.include?("!")
+        #word.split("!").each do |str|
+          #if str !=""
+            #sentence_array<<str
+          #end
+        #end
+      #end
+    #end
   end
 
 end
