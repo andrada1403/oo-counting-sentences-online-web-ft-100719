16895eeadd269ffa8d9aefc315d30a1d4493ea2a
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-12 14:58:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,19 +27,8 @@ class String
   end
 
   def count_sentences
-    #sentence_array=self.split(".")
-    #sentence_array.each do |word|
-      #if word.include?("!")
-        #word.split("!").each do |str|
-          #if str !=""
-            #sentence_array<<str
-          #end
-        #end
-      #end
-    #end
-    def count_sentences
   self.split(/[.?!]+/).grep(/\S/).count
-end
+
   end
 
 end
